5e28a7184fe8a3e0bc2cde4aa11936fd9ed7aef2
Park Bum Jin <orion174@naver.com>
2024-12-25 20:01:47 +0900

    Fix DB

--- src/main/resources/cpms_local_insert.sql
+++ src/main/resources/cpms_local_insert.sql
@@ -41,44 +41,54 @@ VALUES
 INSERT INTO cpms_local.cpms_company
 (company_id, company_nm, reg_id, reg_dt, udt_id, udt_dt, del_yn, del_id, del_dt)
 VALUES
-    (1, 'CODEIDEA', 1, '2024-11-14', NULL, NULL, 'N', NULL, NULL);
+    (1, '코드아이디어', 1, '2024-11-14', NULL, NULL, 'N', NULL, NULL);
 
 INSERT INTO cpms_local.cpms_company
 (company_id, company_nm, reg_id, reg_dt, udt_id, udt_dt, del_yn, del_id, del_dt)
 VALUES
-    (2, '강남구청', 1, '2024-11-14', NULL, NULL, 'N', NULL, NULL);
+    (2, 'Heritage', 1, '2024-11-14', NULL, NULL, 'N', NULL, NULL);
 
 INSERT INTO cpms_local.cpms_company
 (company_id, company_nm, reg_id, reg_dt, udt_id, udt_dt, del_yn, del_id, del_dt)
 VALUES
-    (3, '중랑구청', 1, '2024-11-14', NULL, NULL, 'N', NULL, NULL);
+    (3, '마켓헤머', 1, '2024-11-14', NULL, NULL, 'N', NULL, NULL);
 
 INSERT INTO cpms_local.cpms_project
 (project_id, company_id, project_nm, reg_id, reg_dt, udt_id, udt_dt, del_yn, del_id, del_dt)
 VALUES
-    (1, 2, '강남구청 행정포털', 1, '2024-11-14', NULL, NULL, 'N', NULL, NULL);
+    (1, 2, '헤리티지', 1, '2024-11-14', NULL, NULL, 'N', NULL, NULL);
 
 INSERT INTO cpms_local.cpms_project
 (project_id, company_id, project_nm, reg_id, reg_dt, udt_id, udt_dt, del_yn, del_id, del_dt)
 VALUES
-    (2, 3, '중랑구청 행정포털', 1, '2024-11-14', NULL, NULL, 'N', NULL, NULL);
+    (2, 3, '마켓헤머 Web', 1, '2024-11-14', NULL, NULL, 'N', NULL, NULL);
 
 INSERT INTO cpms_local.cpms_project
 (project_id, company_id, project_nm, reg_id, reg_dt, udt_id, udt_dt, del_yn, del_id, del_dt)
 VALUES
-    (3, 3, '중랑구 게시판', 1, '2024-11-14', NULL, NULL, 'N', NULL, NULL);
+    (3, 3, '마켓헤머 App', 1, '2024-11-14', NULL, NULL, 'N', NULL, NULL);
+
+INSERT INTO cpms_local.cpms_project
+(project_id, company_id, project_nm, reg_id, reg_dt, udt_id, udt_dt, del_yn, del_id, del_dt)
+VALUES
+    (4, 3, '마켓헤머 리뉴얼', 1, '2024-11-14', NULL, NULL, 'N', NULL, NULL);
+
+INSERT INTO cpms_local.cpms_project
+(project_id, company_id, project_nm, reg_id, reg_dt, udt_id, udt_dt, del_yn, del_id, del_dt)
+VALUES
+    (5, 3, '헤머넷', 1, '2024-11-14', NULL, NULL, 'N', NULL, NULL);
 
 INSERT INTO cpms_local.cpms_user
 (user_id, auth_type, company_id, del_yn, login_id, login_pw, use_yn, user_dept, user_info, user_nm, user_phone, user_pos, del_dt, del_id, reg_dt, reg_id, udt_dt, udt_id)
 VALUES
-    (1, 'ADMIN', 1, 'N', 'admin', '$2a$10$0Cqt17WWRQFnM2HEI01Q.O2UoxxAsHo6z.tXUl6D42jjEu0ASZXxe', 'Y', '관리자', '관리자', 'admin', '01012341234', '관리자', NULL, NULL, '2024-10-06', 1, NULL, NULL);
+    (1, 'ADMIN', 1, 'N', 'admin', '$2a$10$0Cqt17WWRQFnM2HEI01Q.O2UoxxAsHo6z.tXUl6D42jjEu0ASZXxe', 'Y', '관리자', '관리자', 'admin', '01062588390', '관리자', NULL, NULL, '2024-10-06', 1, NULL, NULL);
 
 INSERT INTO cpms_local.cpms_user
 (user_id, auth_type, company_id, del_yn, login_id, login_pw, use_yn, user_dept, user_info, user_nm, user_phone, user_pos, del_dt, del_id, reg_dt, reg_id, udt_dt, udt_id)
 VALUES
-    (2, 'USER', 2, 'N', 'orion174', '$2a$10$0Cqt17WWRQFnM2HEI01Q.O2UoxxAsHo6z.tXUl6D42jjEu0ASZXxe', 'Y', '스마트행정과', '행정포털 담당자', '박범진', '01012341234', '7급', NULL, NULL, '2024-11-14', 1, NULL, NULL);
+    (2, 'USER', 2, 'N', 'orion174', '$2a$10$0Cqt17WWRQFnM2HEI01Q.O2UoxxAsHo6z.tXUl6D42jjEu0ASZXxe', 'Y', '헤리티지', '개발 테스트', '박범진', '01062588390', '팀원', NULL, NULL, '2024-11-14', 1, NULL, NULL);
 
 INSERT INTO cpms_local.cpms_user
 (user_id, auth_type, company_id, del_yn, login_id, login_pw, use_yn, user_dept, user_info, user_nm, user_phone, user_pos, del_dt, del_id, reg_dt, reg_id, udt_dt, udt_id)
 VALUES
-    (3, 'USER', 3, 'N', 'orion175', '$2a$10$0Cqt17WWRQFnM2HEI01Q.O2UoxxAsHo6z.tXUl6D42jjEu0ASZXxe', 'Y', '행정지원과', '담당자', '홍길동', '01012341234', '9급', NULL, NULL, '2024-11-14', 1, NULL, NULL);
+    (3, 'USER', 2, 'N', 'orion175', '$2a$10$0Cqt17WWRQFnM2HEI01Q.O2UoxxAsHo6z.tXUl6D42jjEu0ASZXxe', 'Y', '마켓헤머', '개발 테스트', '박범진', '01062588390', '팀원', NULL, NULL, '2024-11-14', 1, NULL, NULL);
